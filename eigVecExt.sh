aa052885ee8a6772fc136eceadd3fbe936a69492
isaac-perez7053 <isacvillages@gmail.com>
2024-07-03 09:06:48 -0700

    Add files via upload

---
+++ eigVecExt.sh
@@ -0,0 +1,55 @@
+#!/bin/bash 
+################################
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <arg1>"
+    exit 1
+fi
+
+######################################################
+# Read input files and calculate constant quantities #
+######################################################
+
+
+## Read the command line arguments
+dynFreqs_file="$1"
+fceVecs_file="$2"
+input_file="$3"
+
+####################################
+# Check which vectors are unstable #
+####################################
+
+sed '1d' "$dynFreqs_file" > "tmpfile.abi" && mv "tmpfile.abi" "$dynFreqs_file" 
+eigVec_lines=$(grep -n "-" "$dynFreqs_file" | cut -d: -f1)
+eigVec_nlines=$(echo "$eigVec_lines" | wc -l )
+eigVec_nlines="${eigVec_nlines#"${eigVec_nlines%%[![:space:]]*}"}" 
+echo "There are "$eigVec_nlines" unstable phonons" 
+
+#####################################
+# Create input files with eigenVecs #
+#####################################
+
+for files in $eigVec_lines
+do
+   cp "../$input_file" "${input_file}_vec${files}"
+   echo -e "vecNum ${files}" >> "${input_file}_vec${files}"
+   eigVec=$(sed -n "${files}p" "$fceVecs_file")
+   echo "eigen_disp" >> "${input_file}_vec${files}"
+   eigVec_arr=()
+   #Input eigVec into array
+   for eigVec in ${eigVec}
+   do
+      eigVec_arr+=("$eigVec")  
+   done
+   #Construct matrix line and input in file 
+   for (( i=0; i<${#eigVec_arr[@]}; i+=3 ))
+   do
+      line="${eigVec_arr[i]} ${eigVec_arr[$(( i+1))]} ${eigVec_arr[$(( i+2))]}"
+      echo "${line}" >> "${input_file}_vec${files}"  
+   done 
+done 
+
+echo "eigVec_nlines $eigVec_nlines" >> "../$input_file"
+echo -e "eigVec_lines\n${eigVec_lines}" >> "../$input_file"
+  
---
+++ flpzPert.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+###############################
+# Executable for flpz program #
+###############################
+
+# Check for correct number of arguments
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <arg1>"
+    exit 1
+fi
+
+echo "Beginning flpz Program"
+
+######################################################
+# Read input files and calculate constant quantities #
+######################################################
+
+## Read the command line arguments
+input_file="$1"
+general_structure_file="$(grep "genstruc" "$input_file" | awk '{print $2}')"
+structure="$(grep "name" "$input_file" | awk '{print $2}')"
+smodes_input="$2"
+irrep="$3"
+ntypat=$(grep "ntypat" "$general_structure_file" | awk '{print $2}')
+
+#####################################################
+# Creation of new working directory and input files #
+#####################################################
+
+# Main working directory for calculations
+dir="${structure}_${irrep}"
+mkdir "$dir"
+
+# Generation of boilerplate for Prof. Ritz's code
+bash flpz/boilerplate_generation.sh "$input_file"
+mv boilerplate "$dir"
+
+# Copy code into new working directory
+cp flpz/boilerplate_generation.sh "$dir"/.
+cp flpz/smodes_symmadapt_abinit.py "$dir"/.
+cp flpz/loop_smodes.tcsh "$dir"/.
+cp flpz/smodes_postproc_abinit.py "$dir"/.
+
+### Begin Working in new directory ###
+cd "$dir"
+
+# Processing of smodes and FCEvecs using smodes program
+python3 smodes_symmadapt_abinit.py "../$smodes_input" "$irrep"
+# Creates job scripts for FCEvec calculations
+tcsh loop_smodes.tcsh "$irrep"
+
+# Copy pseudopotentials into SMODES_irrep for abi cacluations
+for ntypat in $(seq 2 $(( ntypat + 1)) )
+do
+   pseudos="$(grep "pseudos" "../$general_structure_file" | awk "{print \$$ntypat}" | sed 's/[,"]//g')"
+   cp boilerplate/"$pseudos" SMODES_"$irrep"/.
+done
+
+echo "Successfully Created Dependencies"
+echo "FCEvec Calculations Begin"
+
+####################################################################
+# Stores job PIDS into array and waits for all processes to finish #
+####################################################################
+job_ids=()
+file_path=joblist
+
+count=0
+while IFS= read -r line
+do
+    cd "SMODES_${irrep}/dist_${count}"
+   # Submit job and capture the job ID
+    job_id=$(sbatch jobscript.sh | awk '{print $4}')
+    job_ids+=($job_id)
+    echo "Submitted batch job $job_id"
+    cd -
+    count=$(( count + 1 ))
+done < "$file_path"
+
+# Wait for all jobs to finish
+for job_id in "${job_ids[@]}"; do
+    squeue -h -j "$job_id"
+    while [ $? -eq 0 ]; do
+        sleep 60  # Check every minute
+        squeue -h -j "$job_id"
+    done
+    echo "Job $job_id completed"
+done
+
+echo "All Batch Scripts have Completed"
+echo "Post Processing of FCEvecs Begin"
+
+###############################
+#  Post Processing of FCEvecs #
+###############################
+
+# Process smodes output
+python3 smodes_postproc_abinit.py "$irrep"
+mv "SMODES_${irrep}/FCEvecs.dat" "."
+mv "SMODES_${irrep}/DynFreqs.dat" "."
+
+# Take unstable eigenvectors and cat to input
+bash ../eigVecExt.sh "DynFreqs.dat" "FCEvecs.dat" "$input_file"
+
+# Store outputs of previous script
+eigVec_nlines=$(grep "eigVec_nlines" "../$input_file" | awk '{print $2}')
+
+# Put eigenvectors in the right place for calculation of datapoints
+mode_location=$(grep -n "eigVec_lines" "../$input_file" | cut -d: -f1)
+begin_mode=$(( mode_location+1 ))
+end_mode=$(( mode_location+$eigVec_nlines ))
+
+## Extract lines from begin_mode to end_mode and normalize in the process 
+eigVec_lines=$(sed -n "${begin_mode},${end_mode}p" "../$input_file")
+
+echo "Post Processing of FCEvecs is Complete"
+echo "Calculation of Datapoints Begin"
+
+#############################
+# Calculation of datapoints #
+#############################
+
+# Execute all input files generated
+for eigVec in ${eigVec_lines}
+do
+   bash ../datapointCalcofElec.sh "${input_file}_vec${eigVec}"
+done
+
+echo "flpz Program has Completed Calculations"
+
