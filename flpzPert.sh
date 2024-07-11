bb0a106c96a418d7c54c0404864cc4479e7fea29
isaac-perez7053 <isacvillages@gmail.com>
2024-07-11 14:42:48 -0700

    Add files via upload
    Added a new variable for the user calc_phon and num_phon that allows the user to specify which phonons they want calculated. I also copy the input file into the working directory of code so as to not mess with the users input file.

--- flpzPert.sh
+++ flpzPert.sh
@@ -37,10 +37,14 @@ cp flpz_code/boilerplate_generation.sh "$dir"/.
 cp flpz_code/smodes_symmadapt_abinit.py "$dir"/.
 cp flpz_code/loop_smodes.tcsh "$dir"/.
 cp flpz_code/smodes_postproc_abinit.py "$dir"/.
-cp flpz_code/eigVecExt.sh "$dir"/.
+cp flpz_code/eigVecExtElec.sh "$dir"/.
 cp flpz_code/datapointCalcofElec.sh "$dir"/.
 cp flpz_code/dataAnalysis.sh "$dir"/.
 cp flpz_code/xredToxcart.sh "$dir"/.
+cp "$input_file" "$dir"/.
+cp "$smodes_input" "$dir"/.
+cp flpz_code/findAngle.py "$dir"/.
+cp flpz_code/findSpaceGroup.sh "$dir"/.
 
 # Generation of boilerplate for Prof. Ritz's code
 bash "$dir"/boilerplate_generation.sh "$input_file"
@@ -50,7 +54,7 @@ mv boilerplate "$dir"
 cd "$dir"
 
 # Processing of smodes and FCEvecs using smodes program
-python3 smodes_symmadapt_abinit.py "../$smodes_input" "$irrep"
+python3 smodes_symmadapt_abinit.py "$smodes_input" "$irrep"
 # Creates job scripts for FCEvec calculations
 tcsh loop_smodes.tcsh "$irrep"
 
@@ -119,7 +123,16 @@ awk -v new="$new_content" '
     next
 }
 {print}
-' "../$input_file" > "../${input_file}.tmp" && mv "../${input_file}.tmp" "../$input_file"
+' "$input_file" > "${input_file}.tmp" && mv "${input_file}.tmp" "$input_file"
+
+# Report spacegroup of SMODES basis cell
+echo "The space group of the unperturbed cell is "$(bash findSpaceGroup.py $general_structure_file)"" 
+
+# Report space of perturbed cell if datapoints greater than 0 
+if [[ -gt $num_datapoints 0 ]]
+then
+        echo "The space group of the perturbed cell is "$(bash findSpaceGroup.py "SMODES_${irrep}/dist_1/dist_1.abi")""
+fi
 
 # Convert xred to xcart for human readability
 bash xredToxcart.sh "SMODES_${irrep}/dist_0/dist_0.abi"
@@ -130,18 +143,18 @@ mv "SMODES_${irrep}/FCEvecs.dat" "."
 mv "SMODES_${irrep}/DynFreqs.dat" "."
 
 # Take unstable eigenvectors and cat to input
-bash eigVecExt.sh "DynFreqs.dat" "FCEvecs.dat" "$input_file"
+bash eigVecExtElec.sh "DynFreqs.dat" "FCEvecs.dat" "$input_file"
 
 # Store outputs of previous script
-eigVec_nlines=$(grep "eigVec_nlines" "../$input_file" | awk '{print $2}')
+eigVec_nlines=$(grep "eigVec_nlines" "$input_file" | awk '{print $2}')
 
 # Put eigenvectors in the right place for calculation of datapoints
-mode_location=$(grep -n "eigVec_lines" "../$input_file" | cut -d: -f1)
+mode_location=$(grep -n "eigVec_lines" "$input_file" | cut -d: -f1)
 begin_mode=$(( mode_location+1 ))
 end_mode=$(( mode_location+$eigVec_nlines ))
 
 ## Extract lines from begin_mode to end_mode and normalize in the process 
-eigVec_lines=$(sed -n "${begin_mode},${end_mode}p" "../$input_file")
+eigVec_lines=$(sed -n "${begin_mode},${end_mode}p" "$input_file")
 
 echo "Post Processing of FCEvecs is Complete"
 echo "Calculation of Datapoints Begin"
