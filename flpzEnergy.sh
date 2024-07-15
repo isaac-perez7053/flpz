7ef8d5788e39ddf5e52f3ba86d1d3457f8bfd007
isaac-perez7053 <isacvillages@gmail.com>
2024-07-15 15:53:59 -0700

    Add files via upload
    Uploading final files that were used to test the final stages of the program. Small changes revolve around file management and maintainability of code.

--- flpzEnergy.sh
+++ flpzEnergy.sh
@@ -1,87 +1,85 @@
 #!/bin/bash
-###############################
-# Executable for flpz program #
-###############################
 
-# Check for correct number of arguments
+# FLPZ Energy Program
+# Executes the FLPZ (Flexoelectric Piezoelectric) energy calculations
+
+# Usage: ./flpzEnergy.sh <input_file> <smodes_input> <irrep>
 
 if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <arg1>"
+    echo "Usage: $0 <input_file> <smodes_input> <irrep>"
     exit 1
 fi
 
-echo "Beginning flpz Energy Program"
+echo "Beginning FLPZ Energy Program"
 
-######################################################
-# Read input files and calculate constant quantities #
-######################################################
-
-## Read the command line arguments
+# Read input files and calculate constant quantities
 input_file="$1"
-general_structure_file="$(grep "genstruc" "$input_file" | awk '{print $2}')"
-structure="$(grep "name" "$input_file" | awk '{print $2}')"
 smodes_input="$2"
 irrep="$3"
+
+general_structure_file=$(grep "genstruc" "$input_file" | awk '{print $2}')
+structure=$(grep "name" "$input_file" | awk '{print $2}')
 ntypat=$(grep "ntypat" "$general_structure_file" | awk '{print $2}')
 
-#####################################################
-# Creation of new working directory and input files #
-#####################################################
-
-# Main working directory for calculations
+# Create new working directory
 dir="${structure}_${irrep}_Energy"
-mkdir "$dir"
+mkdir -p "$dir"
 
-# Copy code into new working directory
-cp flpz_code/boilerplate_generation.sh "$dir"/.
-cp flpz_code/smodes_symmadapt_abinit.py "$dir"/.
-cp flpz_code/loop_smodes.tcsh "$dir"/.
-cp flpz_code/smodes_postproc_abinit.py "$dir"/.
-cp flpz_code/eigVecExtEnergy.sh "$dir"/.
-cp flpz_code/datapointCalcofEnergy.sh "$dir"/.
-cp flpz_code/dataAnalysisEnergy.sh "$dir"/.
-cp flpz_code/xredToxcart.sh "$dir"/.
-cp "$input_file" "$dir"/.
-cp "$smodes_input" "$dir"/.
-cp flpz_code/findAngle.py "$dir"/.
-cp flpz_code/findSpaceGroup.sh "$dir"/.
+# Copy necessary files to working directory
+handle_files() {
+    local source_dir="$1"
+    local files=(
+        "boilerplate_generation.sh"
+        "smodes_symmadapt_abinit.py"
+        "loop_smodes.tcsh"
+        "smodes_postproc_abinit.py"
+        "eigVecExtEnergy.sh"
+        "datapointCalcofEnergy.sh"
+        "dataAnalysisEnergy.sh"
+        "xredToxcart.sh"
+        "findAngle.py"
+        "findSpaceGroup.sh"
+    )
+    for file in "${files[@]}"; do
+        if [ "$2" = "rm" ]; then
+            rm "$source_dir/$file"
+        elif [ "$2" = "cp" ]; then
+            cp "$source_dir/$file" "$dir/"
+        fi
+    done
+}
 
-# Generation of boilerplate for Prof. Ritz's code
-bash "$dir"/boilerplate_generation.sh "$input_file"
+handle_files "flpz_code" "cp"
+cp "$input_file" "$dir/"
+cp "$smodes_input" "$dir/"
+
+# Generate boilerplate
+bash "$dir/boilerplate_generation.sh" "$input_file"
 mv boilerplate "$dir"
 
-### Begin Working in new directory ###
+# Change to working directory
 cd "$dir"
 
-# Processing of smodes and FCEvecs using smodes program
+# Process smodes and FCEvecs
 python3 smodes_symmadapt_abinit.py "$smodes_input" "$irrep"
-# Creates job scripts for FCEvec calculations
 tcsh loop_smodes.tcsh "$irrep"
 
-# Copy pseudopotentials into SMODES_irrep for abi cacluations
-for ntypat in $(seq 2 $(( ntypat + 1)) )
-do
-   pseudos="$(grep "pseudos" "../$general_structure_file" | awk "{print \$$ntypat}" | sed 's/[,"]//g')"
-   cp boilerplate/"$pseudos" SMODES_"$irrep"/.
-done
+# Copy pseudopotentials
+copy_pseudos() {
+    local dest_dir=$1
+    for i in $(seq 2 $((ntypat + 1))); do
+        pseudos=$(grep "pseudos" "../$general_structure_file" | awk "{print \$$i}" | sed 's/[,"]//g')
+        cp "boilerplate/$pseudos" "$dest_dir/"
+    done
+}
 
-# Copy pseudopotentials into Calculation for abi cacluations
-for ntypat in $(seq 2 $(( ntypat + 1)) )
-do
-   pseudos="$(grep "pseudos" "../$general_structure_file" | awk "{print \$$ntypat}" | sed 's/[,"]//g')"
-   cp boilerplate/"$pseudos" .
-done
+copy_pseudos "SMODES_$irrep"
+copy_pseudos "."
 
-#NOTE: Leave it up to user to correctly put pseudopotentials in flpz directory and directory
-#before. 
+echo "Successfully created dependencies"
+echo "FCEvec calculations begin"
 
-
-echo "Successfully Created Dependencies"
-echo "FCEvec Calculations Begin"
-
-####################################################################
-# Stores job PIDS into array and waits for all processes to finish #
-####################################################################
+# Wait for jobs to finish 
 job_ids=()
 file_path=joblist
 
@@ -107,67 +105,38 @@ for job_id in "${job_ids[@]}"; do
     echo "Job $job_id completed"
 done
 
-echo "All Batch Scripts have Completed"
-echo "Post Processing of FCEvecs Begin"
+echo "All batch scripts have completed"
+echo "Post-processing of FCEvecs begins"
 
-###############################
-#  Post Processing of FCEvecs #
-###############################
+# Post-processing of FCEvecs
 
-# Reassign genStruc as boilerplate base calculation. 
-new_content="genstruc SMODES_${irrep}/dist_0/dist_0.abi"
+# Reassign SMODES as the genstruc file
+sed -i "s|^genstruc .*|genstruc SMODES_${irrep}/dist_0/dist_0.abi|" "$input_file"
+general_structure_file=$(grep "genstruc" "$input_file" | awk '{print $2}')
 
-awk -v new="$new_content" '
-/genstruc/ {
-    print new
-    next
-}
-{print}
-' "$input_file" > "${input_file}.tmp" && mv "${input_file}.tmp" "$input_file"
 
-# Report spacegroup of SMODES basis cell
-echo "The space group of the unperturbed cell is "$(bash findSpaceGroup.py $general_structure_file)"" 
+echo "The space group of the unperturbed cell is $(bash findSpaceGroup.sh $general_structure_file)"
 
-# Report space of perturbed cell if datapoints greater than 0 
-if [[ -gt $num_datapoints 0 ]]
-then 
-        echo "The space group of the perturbed cell is "$(bash findSpaceGroup.py "SMODES_${irrep}/dist_1/dist_1.abi")""
-fi
+bash xredToxcart.sh "SMODES_${irrep}/dist_0/dist_0.abi"
 
-# Convert xred to xcart for human readability 
-bash xredToxcart.sh  "SMODES_${irrep}/dist_0/dist_0.abi"
-
-# Process smodes output
 python3 smodes_postproc_abinit.py "$irrep"
 mv "SMODES_${irrep}/FCEvecs.dat" "."
 mv "SMODES_${irrep}/DynFreqs.dat" "."
 
-# Take unstable eigenvectors and cat to input
 bash eigVecExtEnergy.sh "DynFreqs.dat" "FCEvecs.dat" "$input_file"
 
-# Store outputs of previous script
 eigVec_nlines=$(grep "eigVec_nlines" "$input_file" | awk '{print $2}')
-
-# Put eigenvectors in the right place for calculation of datapoints
 mode_location=$(grep -n "eigVec_lines" "$input_file" | cut -d: -f1)
-begin_mode=$(( mode_location+1 ))
-end_mode=$(( mode_location+$eigVec_nlines ))
+eigVec_lines=$(sed -n "$((mode_location+1)),$((mode_location+eigVec_nlines))p" "$input_file")
 
-## Extract lines from begin_mode to end_mode and normalize in the process 
-eigVec_lines=$(sed -n "${begin_mode},${end_mode}p" "$input_file")
+echo "Post-processing of FCEvecs is complete"
+echo "Calculation of datapoints begins"
 
-echo "Post Processing of FCEvecs is Complete"
-echo "Calculation of Datapoints Begin"
-
-#############################
-# Calculation of datapoints #
-#############################
-
-# Execute all input files generated
-for eigVec in ${eigVec_lines}
-do
-   bash datapointCalcofEnergy.sh "${input_file}_vec${eigVec}"
+# Calculate datapoints
+for eigVec in $eigVec_lines; do
+    bash datapointCalcofEnergy.sh "${input_file}_vec${eigVec}"
 done
 
-echo "flpz Program has Completed Calculations"
+handle_files "./" "rm" 
 
+echo "FLPZ Program has completed calculations"
