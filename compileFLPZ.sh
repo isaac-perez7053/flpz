fc4410ae3c2d7c86a318075494f8776368d928ea
isaac-perez7053 <isacvillages@gmail.com>
2024-08-06 19:20:28 -0700

    Add files via upload

--- compileFLPZ.sh
+++ compileFLPZ.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+# This script is used to resolve any discrepancy with the use of SLURM on different computers,
+# allowing the user to customize their SLURM preamble. Although the first argument is redundant,
+# it allows the code to be used in a general context.
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <inputCode> <b_script_preamble>"
+    exit 1
+fi
+
+# Handle inputs
 inputCode="$1"
 bScriptPreamble="$2"
 
-# Directory to check and create
+# Directory to check and create edited input files
 dir_name="flpz_input_compiled"
 
 # Check if directory exists, remove if it does, then create it
@@ -26,8 +36,8 @@ files_to_process=(
 insert_file() {
     local target_file="$1"
     local temp_file="${target_file}.temp"
-    
-    sed "1r $file_to_insert" "$target_file" > "$temp_file" && mv "$temp_file" "$target_file"
+
+    sed "1r $file_to_insert" "$target_file" >"$temp_file" && mv "$temp_file" "$target_file"
 }
 
 # Process each file
