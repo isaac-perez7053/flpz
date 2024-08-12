fa621f01fa49e97fdda8dcb828ef61bc0d7c19ca
Isaac Perez <iperez@g.hmc.edu>
2024-08-11 18:47:17 -0700

    Sed error

--- flpz_code/shared/xcartToxred.sh
+++ flpz_code/shared/xcartToxred.sh
@@ -114,5 +114,5 @@ update_input_file() {
 check_args "$@"
 input_file="$1"
 extract_data "$input_file"
-xred=$(calculate_xcart "xred" "$rprim" "$xcart" "$a" "$b" "$c" "$natom" | sed 's/\[\]//g' )
+xred=$(calculate_xcart "xred" "$rprim" "$xcart" "$a" "$b" "$c" "$natom" | sed 's/\[//g; s/\]//g' )
 update_input_file "$input_file" "$xred" "xred"
